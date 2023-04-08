38226f3d7249cb5c5a94c433eed48ebd07b1cadc
Rizki Ramadhan <rizkiramdhanx@gmail.com>
2023-04-08 13:57:08 +0700

    improvement : add prisma

--- src/utils/jwtGenerate.ts
+++ src/utils/jwtGenerate.ts
@@ -1,24 +1,23 @@
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+// import jwt from 'jsonwebtoken';
+// import dotenv from 'dotenv';
+// import ENV from '../config/env';
 
-dotenv.config();
+// const ACCESS_TOKEN = ENV.ACCESS_TOKEN as string;
+// const REFRESH_TOKEN = ENV.REFRESH_TOKEN as string;
 
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN || 'cuaks';
-const REFRESH_TOKEN = process.env.REFRESH_TOKEN || 'cuaks';
+// const jwtGenerate = (data: any) => {
+//   const token = jwt.sign(data, ACCESS_TOKEN, {
+//     expiresIn: '1h',
+//   });
 
-const jwtGenerate = (data: any) => {
-  const token = jwt.sign(data, ACCESS_TOKEN, {
-    expiresIn: '1h',
-  });
+//   const refresh = jwt.sign(data, REFRESH_TOKEN, {
+//     expiresIn: '30d',
+//   });
 
-  const refresh = jwt.sign(data, REFRESH_TOKEN, {
-    expiresIn: '30d',
-  });
+//   return {
+//     token: token,
+//     refresh_token: refresh,
+//   };
+// };
 
-  return {
-    token: token,
-    refresh_token: refresh,
-  };
-};
-
-export default jwtGenerate;
+// export default jwtGenerate;
